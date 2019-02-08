54efe550df132b33d2d647fc1be1fe631a7fb5f1
Saroumoun <bastien.hiel@gmail.com>
2019-02-08 16:08:00 +0100

    added gossips index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
+  root 'gossips#index'
+  
+  get 'gossips/index'
+  get 'gossips/show'
+  get 'gossips/new'
+  get 'gossips/create'
+  get 'gossips/edit'
+  get 'gossips/update'
+  get 'gossips/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :gossips
 end
